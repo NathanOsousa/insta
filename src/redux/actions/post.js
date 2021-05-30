69f2a448f0d0f191f308514d311378898e6c6322
nathan de oliveira <nosousa.2000@gmail.com>
2021-05-30 12:27:01 -0300

    feat: add post integrated with firebase

--- src/redux/actions/post.js
+++ src/redux/actions/post.js
@@ -4,13 +4,30 @@ import axios from 'axios';
 
 export const addPost = post => {
   return dispatch => {
-    axios
-      .post('/posts.json', {...post})
-      .then(response => {
-        console.log('🚀 ~ file: post.js ~ line 9 ~ response', response);
+    axios({
+      url: 'uploadImage',
+      baseURL: 'https://insta-71514-default-rtdb.firebaseio.com',
+      method: 'post',
+      data: {
+        image: post.image.base64,
+      },
+    })
+      .then(res => {
+        post.image = res.data.imageUrl;
+        axios
+          .post('/posts.json', {...post})
+          .then(response => {
+            console.log('🚀 ~ file: post.js ~ line 9 ~ response', response);
+          })
+          .catch(error => {
+            console.log(
+              '🚀 ~ file: post.js ~ line 8 ~ axios.post ~ error',
+              error,
+            );
+          });
       })
-      .catch(error => {
-        console.log('🚀 ~ file: post.js ~ line 8 ~ axios.post ~ error', error);
+      .catch(err => {
+        console.log('🚀 ~ file: post.js ~ line 29 ~ err', err);
       });
   };
   // return {
